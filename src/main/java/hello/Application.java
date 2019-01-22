d72fd6abd7631d3b15eb23b1282980cd20ac3042
Dmitrii Fedotov <Dmitrii_Fedotov@epam.com>
2019-01-23 01:20:12 +0400

    Broken commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         //change2
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AApplication.class, args);
     }
 
 }
