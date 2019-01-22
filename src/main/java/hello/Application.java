2e45be731a6193a72465e4d86e258817be55f745
Dmitrii Fedotov <Dmitrii_Fedotov@epam.com>
2019-01-23 01:35:46 +0400

    Repair

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         //change2
-        SpringApplication.run(ASpplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
