61899cb9deaa1a14780532c405e99e53ba04450c
Dmitrii Fedotov <Dmitrii_Fedotov@epam.com>
2019-01-23 00:06:48 +0400

    Third commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        //change
+        //change1
         SpringApplication.run(Application.class, args);
     }
 
